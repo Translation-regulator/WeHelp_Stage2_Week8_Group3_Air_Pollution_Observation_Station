a89b1ef4c2549258032bb25d6304f90bf27702d7
bianliao <138651187+bianliao@users.noreply.github.com>
2025-04-30 16:58:58 +0800

    render siteSelect and set default county

--- static/js/feature/createPreviousSelect.js
+++ static/js/feature/createPreviousSelect.js
@@ -1,7 +1,6 @@
 import { getCountyAndStation } from "../function/getCountyAndStation.js";
 
 export async function createPreviousSelect(){
-
     const countySelect = document.getElementById('county-select');
 
     // 建立選項
@@ -14,40 +13,37 @@ export async function createPreviousSelect(){
         countySelect.appendChild(option);
     });
 
+    // 預選縣市
+    const defaultCounty = '新北市';
+    countySelect.value = defaultCounty;
+    await renderSiteChip({ target: { value: defaultCounty } });
+
     // 監聽選擇的縣市
     countySelect.addEventListener("change", async (event) => {
-        // 取得縣市資料
-        const selectedCounty = event.target.value;
-        const stationData = await getCountyAndStation({ county: selectedCounty });
-        console.log("該縣市的測站資料：", stationData);
-
-        // 取得容器DOM
-        let siteSelect = document.getElementById("site-select");
-
-        // 若有舊的SniteDom，要先移除
-        const oldSiteDom = siteSelect.querySelector(".site-select-option");
-        if (oldSiteDom) {
-            siteSelect.removeChild(oldSiteDom);
-        }
-
-        // 渲染該縣市的監測站
-        let newSiteData = document.createElement("button");
-        newSiteData.className = "site-select-option";
-        stationData.forEach((item) => {
-            let newSiteData = document.createElement("button");
-            newSiteData.textContent = item.sitename;
-            newSiteData.className = "chip text-sm-500 site-select-option";
-            siteSelect.appendChild(newSiteData);
-        });
+        await renderSiteChip(event)
     });
 
-    // // 監聽點擊的監測站，並渲染資料
-    // document
-    //     .getElementById("airDataDom")
-    //     .addEventListener("click", async (event) => {
-    //         if (event.target.classList.contains("airData__station__btn")) {
-    //             const stationName = event.target.textContent;
-    //             console.log("點擊監測站：", stationName);
-    //         }
-    //     });
 }
+
+async function renderSiteChip(event){
+    // 取得縣市資料
+    const selectedCounty = event.target.value;
+    const stationData = await getCountyAndStation({ county: selectedCounty });
+    console.log("該縣市的測站資料：", stationData);
+
+    // 取得容器DOM
+    let siteSelect = document.getElementById("site-select");
+
+    // 若有舊的SiteDom，要先移除
+    siteSelect.innerHTML = "";
+
+    // 渲染該縣市的監測站
+    stationData.forEach((item) => {
+        let newSiteData = document.createElement("button");
+        newSiteData.textContent = item.sitename;
+        newSiteData.className = "chip text-sm-500 site-select-option";
+        siteSelect.appendChild(newSiteData);
+    });
+}
+
+
