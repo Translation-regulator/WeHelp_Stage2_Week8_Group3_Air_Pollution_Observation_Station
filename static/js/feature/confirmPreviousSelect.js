5dac1d2d6064e4f9b8268fa215ec9bc6295ccbec
bianliao <138651187+bianliao@users.noreply.github.com>
2025-04-30 17:54:17 +0800

    change site select

--- static/js/feature/confirmPreviousSelect.js
+++ static/js/feature/confirmPreviousSelect.js
@@ -12,21 +12,12 @@ function initSiteChips() {
     if(!initSiteChipsIsListening){
         siteSelect.addEventListener('click', (event) => {
             const target = event.target;
+            const activeChips = siteSelect.querySelector('.chip-active'); 
 
             if (!target.classList.contains('chip')) return;
 
-            const activeChips = siteSelect.querySelectorAll('.chip-active'); //是否只剩一個
-            const isActive = target.classList.contains('chip-active'); //該選項是否 active
-            console.log(target)
-            console.log(activeChips)
-            console.log(isActive)
-            
-            // 如果目前只有一個 chip-active 且點擊的是它本人，禁止取消
-            if (isActive && activeChips.length === 1 && activeChips[0] === target) {
-                return;
-            }
-
-            target.classList.toggle('chip-active');
+            activeChips.classList.remove('chip-active');
+            target.classList.add('chip-active');
         });
         initSiteChipsIsListening = true;
     }
