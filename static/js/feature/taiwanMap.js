ebce9b2a0df5e0c29c9a45092fe8dac315722497
Conan <changchaocheng870405@gmail.com>
2025-05-02 09:53:40 +0800

    re-Change

--- static/js/feature/taiwanMap.js
+++ static/js/feature/taiwanMap.js
@@ -9,11 +9,7 @@ function taiwanMap() {
   const model = {
     hoverCountry: {},
     clickCountry: {},
-    d3: { svg: {}, topoData: {}, geoData: {}, projection: d3
-        .geoMercator()
-        .center([121, 24]) // 台灣中心的經緯度
-        .scale(8000)
-        .translate([width / 1.5, height / 2]), path: {} },
+    d3: { svg: {}, topoData: {}, geoData: {}, projection: {}, path: {} },
     allStationData: {},
     stationDataByCountry: {},
     statusColor: {
